3ebbc4ce90149601171154b442e8fd0124a77843
BASSAM ALHARBI <101985240+BA-S12@users.noreply.github.com>
2023-11-25 14:57:13 +0300

    Add files via upload

---
+++ MY website/js/main.js
@@ -0,0 +1,6 @@
+const toggleSwitch = document.querySelector('.input');
+
+toggleSwitch.addEventListener('change', () => {
+    document.body.classList.toggle('light-theme');
+    document.body.classList.remove("dark-theme")
+});
