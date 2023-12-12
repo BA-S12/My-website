d101650981c35a33be3034888d9d2203c5975c37
BASSAM ALHARBI <101985240+BA-S12@users.noreply.github.com>
2023-12-12 10:00:28 +0300

    Delete MY website directory

--- MY website/js/main.js
+++
@@ -1,7 +0,0 @@
-const toggleSwitch = document.querySelector('.input');
-
-toggleSwitch.addEventListener('change', () => {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.remove("dark-theme")
-});
-
